42ec5c65b860e8856fc3b3db958860975899d3bc
recipes1520 <velesta.stoyanova@gmail.c>
2014-10-14 15:24:44 -0400

    Implemented google log in. Modified top menu

--- Main.py
+++ Main.py
@@ -2,10 +2,12 @@
 import webapp2
 import os
 
-
 # import the db module from appengine
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+
+# import the users module from appengine
+from google.appengine.api import users
 
 # we'll create a simple Model class here.
 class PostName(db.Model) :  
@@ -17,7 +19,10 @@ class MainPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
    
-    template_values = {}
+    template_values = {
+      'login_btn': getLoginLink(),
+      'logout_btn': getLogoutLink()
+    }
     path = 'templates/index.html'
     self.response.out.write(template.render(path, template_values))
 
@@ -25,8 +30,21 @@ class MainPage(webapp2.RequestHandler) :
 class LoginPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
+    
+    user = users.get_current_user()
+    
+    login = ''
+    logout = ''
+    
+    if user:
+      logout = users.create_logout_url('/')
+    else:
+      login = users.create_login_url('/')
     
-    template_values = {}
+    template_values = {
+      'login': login,
+      'logout': logout
+    }
     path = 'templates/sign-in.html'
     self.response.out.write(template.render(path, template_values))
     
@@ -35,7 +53,10 @@ class ReviewPage(webapp2.RequestHandler) :
   def get(self) :
    
  
-    template_values = {}
+    template_values = {
+      'login_btn': getLoginLink(),
+      'logout_btn': getLogoutLink()
+    }
     path = 'templates/review-form.html'
     self.response.out.write(template.render(path, template_values))
     
@@ -43,7 +64,10 @@ class SubmitPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
    
-    template_values = {}
+    template_values = {
+      'login_btn': getLoginLink(),
+      'logout_btn': getLogoutLink()
+    }
     path = 'templates/recipe-submission.html'
     self.response.out.write(template.render(path, template_values))
     
@@ -55,8 +79,20 @@ class RegisterPage(webapp2.RequestHandler) :
     template_values = {}
     path = 'templates/sign-up.html'
     self.response.out.write(template.render(path, template_values))
-
     
+def getLoginLink():
+  user = users.get_current_user()
+  if not user:
+    return users.create_login_url('/')
+  else:
+    return ''
+  
+def getLogoutLink():
+  user = users.get_current_user()
+  if user:
+    return users.create_logout_url('/')
+  else:
+    return ''
 
 # we use this to set up the AppEngine app - each of the mappings identifies a 
 # URL and the webapp2.RequestHandler class that handles requests to that URL
