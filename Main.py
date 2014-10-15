ed043532187a6b47b84cf82201f202ab7ac1d3e7
Jordan <jpb80@pitt.edu>
2014-10-15 13:59:47 -0400

    review submit updates

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 # import the webapp2 module so we can get access to the framework
+import cgi
 import webapp2
 import os
 
 # import the db module from appengine
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
 
 # import the users module from appengine
@@ -22,15 +23,15 @@ def get_key() :
 
 
 # we'll create a simple Model class here.
-class PostName(db.Model) :  
+class PostName(ndb.Model) :
   # this model class has just one property - myname
-  myname = db.StringProperty()
+  myname = ndb.StringProperty()
 
 # This class is a request handler.
 class MainPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
-   
+
     template_values = {
       'login_btn': getLoginLink(),
       'logout_btn': getLogoutLink()
@@ -79,24 +80,24 @@ class CommentSection(webapp2.RequestHandler) :
 class LoginPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
-    
+
     user = users.get_current_user()
-    
+
     login = ''
     logout = ''
-    
+
     if user:
       logout = users.create_logout_url('/')
     else:
       login = users.create_login_url('/')
-    
+   
     template_values = {
       'login': login,
       'logout': logout
     }
     path = 'templates/sign-in.html'
     self.response.out.write(template.render(path, template_values))
-    
+
 class ReviewPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
@@ -108,18 +109,18 @@ class ReviewPage(webapp2.RequestHandler) :
     }
     path = 'templates/review-form.html'
     self.response.out.write(template.render(path, template_values))
-    
+
 class SubmitPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
   def get(self) :
-   
+
     template_values = {
       'login_btn': getLoginLink(),
       'logout_btn': getLogoutLink()
     }
     path = 'templates/recipe-submission.html'
     self.response.out.write(template.render(path, template_values))
-    
+
     # This class is a request handler.
 class RegisterPage(webapp2.RequestHandler) :
   # implementing the get method here allows this class to handle GET requests.
@@ -128,14 +129,14 @@ class RegisterPage(webapp2.RequestHandler) :
     template_values = {}
     path = 'templates/sign-up.html'
     self.response.out.write(template.render(path, template_values))
-    
+
 def getLoginLink():
   user = users.get_current_user()
   if not user:
     return users.create_login_url('/')
   else:
     return ''
-  
+
 def getLogoutLink():
   user = users.get_current_user()
   if user:
@@ -143,7 +144,7 @@ def getLogoutLink():
   else:
     return ''
 
-# we use this to set up the AppEngine app - each of the mappings identifies a 
+# we use this to set up the AppEngine app - each of the mappings identifies a
 # URL and the webapp2.RequestHandler class that handles requests to that URL
 app = webapp2.WSGIApplication([
   ('/', MainPage),
