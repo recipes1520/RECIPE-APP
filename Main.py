40c922e712e7c82853397078f77da1d5370b1ea5
stevil7 <steven.faurie@gmail.com>
2014-10-27 15:24:47 -0400

    Submit Temporarily Broken
    Working on getting blobstore to work.  temporarily broken.

--- Main.py
+++ Main.py
@@ -1,87 +1,92 @@
 import cgi
 import webapp2
 import os
+import urllib
 
 from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 # Creating datastorage for the user review submissions on review page
 class ReviewSubmission(ndb.Model) :
-    recipe_name = ndb.StringProperty(indexed=False)
-    author = ndb.StringProperty(indexed=False)
-    rating = ndb.IntegerProperty(indexed=False)
-    comment = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+	recipe_name = ndb.StringProperty(indexed=False)
+	author = ndb.StringProperty(indexed=False)
+	rating = ndb.IntegerProperty(indexed=False)
+	comment = ndb.StringProperty(indexed=False)
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
 def get_key() :
-    return ndb.Key('recipe_name', 'author')
+	return ndb.Key('recipe_name', 'author')
 
 class Recipe(ndb.Model) :
-    title = ndb.StringProperty()
-    user_author = ndb.StringProperty()
-    ingredients = ndb.StringProperty(repeated=True)
-    description = ndb.TextProperty()
-    instructions = ndb.StringProperty(repeated=True)
-    prep_time_est = ndb.StringProperty()
-    cook_time_est = ndb.StringProperty()
-    image = ndb.BlobProperty(default=None)
+	title = ndb.StringProperty()
+	user_author = ndb.StringProperty()
+	ingredients = ndb.StringProperty(repeated=True)
+	description = ndb.TextProperty()
+	instructions = ndb.StringProperty(repeated=True)
+	prep_time_est = ndb.StringProperty()
+	cook_time_est = ndb.StringProperty()
+	image = ndb.BlobKeyProperty()
 
 # This class is a request handler for the Main Page.
 class MainPage(webapp2.RequestHandler) :
- 	def get(self) :
- 		template_values = dict()
- 		path = 'templates/index.html'
- 		render_template(self, template_values, path)
+	def get(self) :
+		template_values = dict()
+		path = 'templates/index.html'
+		render_template(self, template_values, path)
 
 class ReviewPage(webapp2.RequestHandler) :
-    def get(self) :
-        self.render_reviews()
+	def get(self) :
+		self.render_reviews()
 
-    def render_reviews(self) :
-        query = ReviewSubmission.query(ancestor=get_key()).order(
-                -ReviewSubmission.date)
-        user_reviews = query.fetch()
-        #unique to your template
-        template_values = {
-            'review_submit' : user_reviews,
-        }
-        path = 'templates/review-form.html'
-        render_template(self, template_values, path)
+	def render_reviews(self) :
+		query = ReviewSubmission.query(ancestor=get_key()).order(
+				-ReviewSubmission.date)
+		user_reviews = query.fetch()
+		#unique to your template
+		template_values = {
+			'review_submit' : user_reviews,
+		}
+		path = 'templates/review-form.html'
+		render_template(self, template_values, path)
 
 class CommentSection(webapp2.RequestHandler) :
-    def post(self) :
-        try :
-            input_recipe = str(cgi.escape(self.request.get('recipe')))
-            input_author = str(users.get_current_user())
-            input_rating = int(cgi.escape(self.request.get('rating')))
-            input_comments = str(cgi.escape(self.request.get('comments')))
-            self.store_comment(input_recipe, input_author, input_rating,
-                           input_comments)
-            self.redirect('/review')
-        except(TypeError, ValueError):
-            self.response.out.write('<html><body>Invalid input</html></body')
+	def post(self) :
+		try :
+			input_recipe = str(cgi.escape(self.request.get('recipe')))
+			input_author = str(users.get_current_user())
+			input_rating = int(cgi.escape(self.request.get('rating')))
+			input_comments = str(cgi.escape(self.request.get('comments')))
+			self.store_comment(input_recipe, input_author, input_rating,
+						   input_comments)
+			self.redirect('/review')
+		except(TypeError, ValueError):
+			self.response.out.write('<html><body>Invalid input</html></body')
 
-    def store_comment(self, input_recipe, input_author, input_rating,
-                      input_comments) :
-        comment = ReviewSubmission(parent=get_key())
-        comment.recipe_name = input_recipe
-        comment.author = input_author
-        comment.rating = input_rating
-        comment.comment = input_comments
-        comment.put()
+	def store_comment(self, input_recipe, input_author, input_rating,
+					  input_comments) :
+		comment = ReviewSubmission(parent=get_key())
+		comment.recipe_name = input_recipe
+		comment.author = input_author
+		comment.rating = input_rating
+		comment.comment = input_comments
+		comment.put()
 
 class SubmitPage(webapp2.RequestHandler) :
-  # implementing the get method here allows this class to handle GET requests.
+	# implementing the get method here allows this class to handle GET requests.
 	def get(self) :
-
-		template_values = {}
+		upload_url = blobstore.create_upload_url('/upload')
+		template_values = {'uploadURL': upload_url}
 		path = 'templates/recipe-submission.html'
 		render_template(self, template_values, path)
 
+class UploadRecipe(blobstore_handlers.BlobstoreUploadHandler):
 	def post(self):
+		pic = self.get_uploads('image')
 		recipe = Recipe(parent=get_key())
+		recipe.image = pic[0]
 		recipe.title = str(cgi.escape(self.request.get('recipe_title')))
 		recipe.user_author= str(users.get_current_user())
 		recipe.ingredients = self.getIngredients()
@@ -90,7 +95,6 @@ class SubmitPage(webapp2.RequestHandler) :
 		recipe.prep_time_est = str(cgi.escape(self.request.get('prep_time'))) #PREP TIME ONLY.  WILL UPDATE WITH + COOK
 		recipe.cook_time_est = str(cgi.escape(self.request.get('cook_time')))
 		recipe.genre = str(cgi.escape(self.request.get('category')))
-        #upload_url = blobstore.create_upload_url('/upload')
 		recipe.put()
 
 		self.redirect('/recipe-submit')
@@ -182,4 +186,5 @@ app = webapp2.WSGIApplication([
   ('/submit_comment', CommentSection),
   ('/search', SearchHandler),
   ('/recipes/(.*)', RecipeDisplay),
+  ('/upload', UploadRecipe),
 ], debug=True)
